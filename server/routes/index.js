4038791a6e900a83a931d3d5cc1f3d3de7430e7a
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2016-04-14 04:37:51 -0400

    Initial commit

---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    var express = require('express');
+    var router = express.Router();
+    var mainController = require('../controllers/mainController');
+
+    /* GET home page. */
+    router.get('/', mainController.index);
+
+    module.exports = router;
+})();
