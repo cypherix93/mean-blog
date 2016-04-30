34124c191e8ef8b8c63bf1e632479d4447429f6b
cypherix93 <bikram.ghosh93@gmail.com>
2016-04-30 17:50:09 -0400

    Further broke up the gulp files into smaller files.

--- gulp/build.js
+++ gulp/build.js
@@ -1,44 +1,8 @@
 "use strict";
 
-var del = require("del");
-var merge = require("merge-stream");
-var runSequence = require("run-sequence");
-
 module.exports = function (gulp, plugins, paths)
-{    
-    // Deploy App
-    gulp.task("deploy", ["build"],
-        function ()
-        {
-            var server = gulp.src(paths.build + "server/**")
-                .pipe(plugins.zip("server.zip", {compress: true}))
-                .pipe(gulp.dest(paths.deploy));
-            
-            var client = gulp.src(paths.build + "client-web/**")
-                .pipe(plugins.zip("client.zip", {compress: true}))
-                .pipe(gulp.dest(paths.deploy));
-            
-            merge(server, client);
-        });
-    
-    // Build App
-    gulp.task("build", function (callback)
-    {
-        runSequence(
-            "clean",
-            ["compile-server", "compile-client"],
-            callback
-        );
-    });
-    
-    // Clean Directories
-    gulp.task("clean",
-        function (callback)
-        {
-            return del([
-                    paths.build + "**",
-                    paths.deploy + "**"
-                ],
-                {force: true}, callback);
-        });
+{
+    require("./build/clean")(gulp, plugins, paths);
+    require("./build/build")(gulp, plugins, paths);
+    require("./build/deploy")(gulp, plugins, paths);
 };
